8dd25e8e90aabdf576179c38f986f4f2dcbd4f82
Zeynep <altay-zeynep@hotmail.com>
2023-03-08 16:53:06 +0300

    Item class

---
+++ Assets/Item.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+static void Main(string[] args)
+    {
+        public string item;
+        Abilities[] passiveAbilities;
+        Abilities[] specialAbilities;
+    
+
+    }
+static void ActiveAbilities()
+{
+    //Stat buff TO DO
+}
+enum Rarity
+{
+    Commen,
+    Uncommen,
+    Epic,
+    Legendary
+}
+}
